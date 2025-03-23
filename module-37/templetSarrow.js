9cc7174a8e246ec5dce56e5bdba290c3cc1485ad
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-23 20:03:13 +0600

    tamplet string arrow fungtion recap

---
+++ module-37/templetSarrow.js
@@ -0,0 +1,24 @@
+// ! 1 tampletString 
+
+let fName = 'tanjil';
+let lName = 'tanvir';
+
+const fullNmae = `my name is ${fName} my brother name is ${lName}`;
+console.log(fullNmae);
+
+
+// ! 2 arrow fungtion
+// console.log(myName(1,2));
+// function myName(d,w){
+//     console.log(arguments);
+//     return(2+2)
+// }
+
+// const names=(...rest)=>{
+// console.log(rest);
+// }
+// names(1,3,4)
+
+const arroww=(a,b,c)=>a+b+c;
+
+console.log(arroww(3,2,2));
