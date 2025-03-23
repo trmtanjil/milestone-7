7082c6ecfdd6c86843fcd4f73058e9a0effd28b9
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-23 23:34:16 +0600

    array and object distructuring commit

---
+++ module-37/arryAndobDistracture.js
@@ -0,0 +1,18 @@
+const infoMane = ['tanjil',{
+    age:22,
+    wordAblt:'medium',
+    personality:true
+}]
+const [name, aged, ] = infoMane;
+console.log(aged);
+
+
+const objectDst={
+    namee:'tanjil',
+    age: 22,
+    work:false,
+    emosion:true,
+    friends:['trm','at','s']
+}
+const {namee,age,word,friends}=objectDst;
+console.log(friends[2]);
