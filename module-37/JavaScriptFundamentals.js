947a96ee6aa0b9ab067ecb61c636cebea359a19f
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-23 17:46:03 +0600

    if else recap

--- module-37/JavaScriptFundamentals.js
+++ module-37/JavaScriptFundamentals.js
@@ -6,3 +6,23 @@
 // const namee = 'tanjil';
 // namee = 'tanvir'
 // console.log(namee);
+
+
+//! 2 if else 
+
+// const number = 220;
+
+// if(number>=20 && number<=40){
+//     console.log('all ok');
+// }
+// else{
+//     console.log('somthing wrong');
+// }
+const number = 220;
+
+if(number>=20 || number<=40){
+    console.log('all ok');
+}
+else{
+    console.log('somthing wrong');
+}
