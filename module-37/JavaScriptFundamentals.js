67a6474b196319313fef8999c7cb503671f1f3b9
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-23 17:55:35 +0600

    array recap

--- module-37/JavaScriptFundamentals.js
+++ module-37/JavaScriptFundamentals.js
@@ -18,11 +18,23 @@
 // else{
 //     console.log('somthing wrong');
 // }
-const number = 220;
+// const number = 220;
 
-if(number>=20 || number<=40){
-    console.log('all ok');
-}
-else{
-    console.log('somthing wrong');
-}
+// if(number>=20 || number<=40){
+//     console.log('all ok');
+// }
+// else{
+//     console.log('somthing wrong');
+// }
+
+//! 3 array
+
+let arrayy = ['trm', 'ttm', 'tnm','at','trm', 'ttm', 'tnm','at']
+// arrayy.push('ttr')
+// arrayy.pop()
+
+// arrayy.unshift('mmr')
+// arrayy.shift()
+
+console.log(arrayy.slice(1,3));
+console.log(arrayy.splice(1,3));
