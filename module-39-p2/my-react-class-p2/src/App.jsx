f5ca2204fc1748f7e45d717e1f333f033d7ec186
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-26 00:31:20 +0600

    differant way to handle event commit

---
+++ module-39-p2/my-react-class-p2/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  function handaling(){
+    alert ("all ok")
+  }
+
+  const handaling3=()=>{
+    alert('click 3')
+  }
+
+const handelAdd5=(num)=>{
+  const newNum = num+5;
+   alert(newNum)
+}
+
+  return (
+    <>
+      <h3>Vite + React</h3>
+     
+   
+   <button onClick={handaling}>click</button>
+   <button style={{margin:'11px'}} onClick={function handaling2(){alert('clicked2')}}>click 2</button>
+   <button onClick={handaling3}>click me 3</button>
+   <button onClick={()=>alert('all ok')}>click me 4</button>
+
+   <button onClick={()=>handelAdd5(1)}>add umber</button>
+    </>
+  )
+}
+
+export default App
