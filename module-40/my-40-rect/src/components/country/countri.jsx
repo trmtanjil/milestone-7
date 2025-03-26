fcb90f4d31885d91e0b7b711331f2539c2877cf2
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-27 02:33:47 +0600

    Conditional CSS and Toggle state based on click commit

--- module-40/my-40-rect/src/components/country/countri.jsx
+++ module-40/my-40-rect/src/components/country/countri.jsx
@@ -1,16 +1,31 @@
- import "./country.css"
+ import { useState } from "react";
+import "./country.css"
 
- 
+
 const Countri = ({country}) => {
- 
-    console.log(country);
+ const [visited, setVisited] = useState(false);
+    const handeleVisited=()=>{
+    //   if(visited ===true){
+    //     setVisited(false);
+    //   }
+    //   else{
+    //     setVisited(true)
+    //   }
+
+   setVisited(!visited)
+    }
     return (
-        <div className="country">
+        <div className={`country ${visited && 'country-visited' }`}>
             <h3>name :{country.name.common
             } </h3>
             <img src={country.flags.png} alt="" /> 
             <p>independent : {country.independent ? ' free': ' not free'}</p>
             <p>country lend : {country.area} sqr mils</p>
+            <button className={visited ? 'btn-visited': 'btn-not-visited'} onClick={handeleVisited}>
+                {
+                    visited ?'visited': 'not visited'
+                }
+            </button>
         </div>
     );
 };
