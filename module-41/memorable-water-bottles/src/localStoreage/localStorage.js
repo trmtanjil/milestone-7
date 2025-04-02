2977e783b2c54265b9b598bce2c49574fbdb2773
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-04-02 16:20:26 +0600

    introduction to local storage Storing data in the vrowser commit

---
+++ module-41/memorable-water-bottles/src/localStoreage/localStorage.js
@@ -0,0 +1,30 @@
+const getCartFromLocalStorage = ()=>{
+    const storedCardString = localStorage.getItem('cart');
+
+    if(storedCardString){
+        const storage = JSON.parse(storedCardString);
+        return storage
+    }
+    return []
+}
+
+const saveCartToLocalStorage= cart =>{
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+
+const AddItemToCartLocalStorage = id=>{
+    const cart = getCartFromLocalStorage();
+    // cart.push(id);
+    const newCart = [...cart, id];
+
+    // save new cart to the local storage
+    saveCartToLocalStorage(newCart)
+
+}
+
+export{
+    getCartFromLocalStorage, 
+    AddItemToCartLocalStorage
+}
